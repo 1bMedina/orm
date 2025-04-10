a4f07d5e0b7b9501332b778c54429c5d9b16c653
Blu Medina <blu.medina.biz@gmail.com>
2025-04-10 08:27:21 -0400

    working on orm faker

--- db/models.py
+++ db/models.py
@@ -1,4 +1,6 @@
 import sys
+from django.db import models
+import uuid
 
 try:
     from django.db import models
@@ -13,4 +15,79 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+class Stoves(models.Model):
+    INTERIOR = 'Int'
+    EXTERIOR = 'Ext'
+    LOCATION_CHOICES = [
+        (INTERIOR, 'Interior'),
+        (EXTERIOR, 'Exterior'),
+    ]
+    BEGINNER = 'Beginner'
+    EXPERT = 'Expert'
+    EXPERIENCE_CHOICES = [
+        (BEGINNER, 'Beginner'),
+        (EXPERT, 'Expert'),
+    ]
+    COLD = 'Cold'
+    HOT = 'Hot'
+    MIXED = 'Mixed'
+    CLIMATE_CHOICES = [
+        (COLD, 'Cold'),
+        (HOT, 'Hot'),
+        (MIXED, 'Mixed'),
+    ]
+    COOKING = 'Cooking'
+    HEATING = 'Heating'
+    OTHER = 'Other'
+    USE_CHOICES = [
+        (COOKING, 'Cooking'),
+        (HEATING, 'Heating'),
+        (OTHER, 'Other'),
+    ]
+
+    #stove_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stove_url = models.URLField()
+    dimensions = models.CharField(max_length=255)
+    experience = models.CharField(max_length=8, choices=EXPERIENCE_CHOICES)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    climate = models.CharField(max_length=5, choices=CLIMATE_CHOICES)
+    stove_location =  models.CharField(max_length=3, choices=LOCATION_CHOICES)
+    use = models.CharField(max_length=7, choices=USE_CHOICES)
+
+def __str__(self):
+    return f" {self.stove_url}|{self.dimensions}|{self.experience}|{self.price}|{self.climate}|{self.stove_location}|{self.use}"
+
+
+class Materials(models.Model):
+    material_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    stove_id = models.ForeignKey(Stoves, on_delete=models.CASCADE)
+    material_name = models.CharField(max_length=255)
+
+class Reviews(models.Model):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    RATING_CHOICES = [
+        (ONE, '1'),
+        (TWO, '2'),
+        (THREE, '3'),
+        (FOUR, '4'),
+        (FIVE, '5'),
+    ]
+    review_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    stove_id = models.ForeignKey(Stoves, on_delete=models.CASCADE)
+    rating = models.CharField(max_length=2, choices=RATING_CHOICES)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class History(models.Model):
+    history_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    stove_id = models.ForeignKey(Stoves, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 
--- main.py
+++ main.py
@@ -19,6 +19,14 @@ django.setup()
 # Import your models for use in your script
 from db.models import *
 
+
+from faker import Faker
+from faker.providers import BaseProvider
+from faker.providers.internet import BaseProvider
+import random
+#from django.contrib.auth.models import User
+from model_bakery.recipe import Recipe
+
 ############################################################################
 ## START OF APPLICATION
 ############################################################################
@@ -44,3 +52,99 @@ User.objects.create(name='Robert')
 
 for u in User.objects.all():
     print(f'ID: {u.id} \tUsername: {u.name}')
+
+
+fake = Faker()
+INT = 'Int'
+EXT = 'Ext'
+LOCATION_CHOICES = [
+        (INT, 'Interior'),
+        (EXT, 'Exterior'),
+]
+BEG = 'Beginner'
+EXP = 'Expert'
+EXPERIENCE_CHOICES = [
+        (BEG, 'Beginner'),
+        (EXP, 'Expert'),
+]
+CL = 'Cold'
+HT = 'Hot'
+MD = 'Mixed'
+CLIMATE_CHOICES = [
+        (CL, 'Cold'),
+        (HT, 'Hot'),
+        (MD, 'Mixed'),
+    ]
+
+COOKING = 'Cooking'
+HEATING = 'Heating'
+OTHER = 'Other'
+USE_CHOICES = [
+        (COOKING, 'Cooking'),
+        (HEATING, 'Heating'),
+        (OTHER, 'Other'),
+    ]
+
+def fake_dimensions(min_val=1.0, max_val=100.0, precision=2):
+    return {
+        'width': round(random.uniform(min_val,max_val), precision),
+        'height': round(random.uniform(min_val,max_val), precision),
+        'depth': round(random.uniform(min_val,max_val), precision)
+    }
+
+for i in range(40):
+    stoves = Stoves(
+        stove_url = fake.domain_name(),
+         dimensions = fake_dimensions(), 
+         experience = random.choice(EXPERIENCE_CHOICES), 
+         price = fake.pydecimal(left_digits = 3, right_digits = 6), 
+         climate = random.choice(CLIMATE_CHOICES), 
+         stove_location = random.choice(LOCATION_CHOICES), 
+         use = random.choice(USE_CHOICES)
+         )
+    stoves.save()
+
+
+materialsName = ["steel", "aluminum", "plastic", "rubber", "glass", "carbon fiber", "wood", "ceramic", "leather"]
+
+for i in range(40):
+    materials = Materials(
+        material_id = fake.uuid4(),
+        material_name = random.choice(materialsName),
+        stove_id = random.choice(Stoves.objects.all())
+    )
+    materials.save()
+
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4
+FIVE = 5
+RATING_CHOICES = [
+    (ONE, '1'),
+    (TWO, '2'),
+    (THREE, '3'),
+    (FOUR, '4'),
+    (FIVE, '5'),
+]
+
+for i in range(40):
+    reviews = Reviews(
+        review_id = fake.uuid4(),
+        user_id = random.choice(User.objects.all()),
+        stove_id = random.choice(Stoves.objects.all()),
+        rating = random.choice(RATING_CHOICES),
+        comment = fake.paragraph(nb_sentences = 3),
+        created_at = fake.date_time()
+    )
+    reviews.save()
+
+
+for i in range(40):
+    history = History(
+        history_id = fake.uuid4(),
+        user_id = random.choice(User.objects.all()),
+        stove_id = random.choice(Stoves.objects.all()),
+        timestamp = fake.date_time()
+    )
+    history.save()
